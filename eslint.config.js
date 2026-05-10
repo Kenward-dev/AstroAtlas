b501d28bcb048aa1263db6b51237850f5d69b014
Kenward Terhemba <codewithkenward@gmail.com>
2026-05-11 00:07:17 +0100

    chore: fix eslint config for v10 compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,23 @@
 import js from "@eslint/js";
-import reactPlugin from "eslint-plugin-react";
+import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   js.configs.recommended,
   {
     plugins: {
-      react: reactPlugin,
       "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
     },
     rules: {
       "no-unused-vars": "warn",
       "no-console": "warn",
-      "react/prop-types": "warn",
-      "react/react-in-jsx-scope": "off",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
-    },
-    settings: {
-      react: { version: "detect" },
     },
   },
 ];
