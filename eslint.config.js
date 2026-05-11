69c7c7e13b83fc13e75d196b6c934036ebde5281
Kenward Terhemba <codewithkenward@gmail.com>
2026-05-11 02:04:32 +0100

    chore: fix eslint glob pattern

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
   js.configs.recommended,
   {
+    files: ["src/**/*.{js,jsx}"],
     plugins: {
       "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     languageOptions: {
       globals: {
         ...globals.browser,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     rules: {
@@ -18,6 +26,7 @@ export default [
       "no-console": "warn",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
+      "react-refresh/only-export-components": "warn",
     },
   },
 ];
