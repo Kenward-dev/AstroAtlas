771fb6a927ec6175db350d91e5360be8b48eb359
Joshua ADEGOKE <skento007@gmail.com>
2026-05-12 15:24:15 +0100

    Format PlanetCard component files

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,25 +1,20 @@
-import "./Planet.css"
-import PropTypes from "prop-types"
+import "./Planet.css";
+import PropTypes from "prop-types";
 
 export default function PlanetCard({ name, distance, image }) {
-
-
-
-    return (
-        <section className="planetCard">
-            <figure>
-                <img src={image} alt={name} />
-                <figcaption>{name}</figcaption>
-                <p>{distance}</p>
-            </figure>
-        </section>
-
-
-    )
+  return (
+    <section className="planetCard">
+      <figure>
+        <img src={image} alt={name} />
+        <figcaption>{name}</figcaption>
+        <p>{distance}</p>
+      </figure>
+    </section>
+  );
 }
 
 PlanetCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    distance: PropTypes.number.isRequired,
-    image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  distance: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
 };
