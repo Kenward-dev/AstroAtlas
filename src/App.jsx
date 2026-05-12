14f787ae2fce34883dbd01f6f3efafef1340b373
Joshua ADEGOKE <skento007@gmail.com>
2026-05-12 14:55:10 +0100

    Create reusable PlanetCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import PlanetCard from "./components/PlanetCard";
+
 function App() {
-  return <div></div>;
+  return <PlanetCard
+    image='https://example.com/image.jpg'
+    name="Jupiter"
+    distance={227.9}
+
+  />;
 }
 
 export default App;
---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,25 @@
+import "./Planet.css"
+import PropTypes from "prop-types"
+
+export default function PlanetCard({ name, distance, image }) {
+
+
+
+    return (
+        <section className="planetCard">
+            <figure>
+                <img src={image} alt={name} />
+                <figcaption>{name}</figcaption>
+                <p>{distance}</p>
+            </figure>
+        </section>
+
+
+    )
+}
+
+PlanetCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    distance: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+};
