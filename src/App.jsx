63ab91d454027582ca7e2a8fdf5cd2adf48365fa
Joshua ADEGOKE <skento007@gmail.com>
2026-05-15 17:09:40 +0100

    Build controlled contact form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Layout from "./components/Layout";
 import PlanetSection from "./components/PlanetSection";
+import ContactForm from "./components/ContactForm";
+
 
 function App() {
   return (
     <Layout>
       <PlanetSection />
+      <ContactForm />
     </Layout>
   );
 }
